0085c27834ca7aa0ab175524c3787fb77c5e07da
raveerocks <raveendra.tudangil@gmail.com>
2021-08-31 17:11:08 +0530

    Initial commit

---
+++ webdriver-framework-junit5/src/main/java/com/browserstack/webdriver/junit5/extensions/WebDriverParameterResolver.java
@@ -0,0 +1,89 @@
+package com.browserstack.webdriver.junit5.extensions;
+
+import com.browserstack.webdriver.config.Platform;
+import com.browserstack.webdriver.core.WebDriverFactory;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+
+/**
+ * {@code WebDriverParameterResolver} is an {@link org.junit.jupiter.api.extension.Extension Extension} of type  {@link org.junit.jupiter.api.extension.ParameterResolver ParameterResolver}.
+ * {@code WebDriverParameterResolver} resolves parameter of type {@link org.openqa.selenium.WebDriver WebDriver} for {@link com.browserstack.webdriver.junit5.extensions.WebDriverTest WebDriverTest}.
+ */
+public class WebDriverParameterResolver implements ParameterResolver {
+
+    public static final ExtensionContext.Namespace STORE_NAMESPACE =
+            ExtensionContext.Namespace.create("com.browserstack.examples");
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebDriverParameterResolver.class);
+
+    private final WebDriverFactory webDriverFactory;
+    private final Platform platform;
+
+    public WebDriverParameterResolver(WebDriverFactory webDriverFactory, Platform platform) {
+        this.webDriverFactory = webDriverFactory;
+        this.platform = platform;
+    }
+
+    /**
+     * Determines if  {@code WebDriverParameterResolver} supports resolution of an argument.
+     *
+     * @param parameterContext the context for the parameter for which an argument should
+     *                         be resolved; never {@code null}.
+     * @param extensionContext the extension context for the {@code Method}
+     *                         about to be invoked; never {@code null}.
+     * @return {@code true} if the {@code parameter} is of type {@link WebDriver}.
+     */
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext,
+                                     ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType().equals(WebDriver.class);
+    }
+
+    /**
+     * Resolves an argument of type {@link WebDriver} in the supplied {@link ParameterContext}
+     * for the supplied {@link ExtensionContext}.
+     *
+     * <p>This method is only called by the framework if {@link #supportsParameter}
+     * previously returned {@code true} for the same {@link ParameterContext}
+     * and {@link ExtensionContext}.
+     *
+     * @param parameterContext the context for the parameter for which an argument should
+     *                         be resolved; never {@code null}
+     * @param extensionContext the extension context for the {@code Executable}
+     *                         about to be invoked; never {@code null}
+     * @return the resolved argument for the {@code parameter} of type {@link WebDriver};
+     * @throws ParameterResolutionException is thrown when {@link WebDriver} initialisation fails.
+     */
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext,
+                                   ExtensionContext extensionContext) throws ParameterResolutionException {
+        String testMethodName = extensionContext.getDisplayName();
+        String[] specificCapabilities = new String[0];
+        WebDriverTest webDriverTest = extensionContext.getTestMethod().get().getAnnotation(WebDriverTest.class);
+        if (webDriverTest != null) {
+            specificCapabilities = webDriverTest.capabilities();
+        }
+        WebDriver webDriver = createWebDriver(testMethodName, specificCapabilities);
+        if (webDriver == null) {
+            throw new ParameterResolutionException("Unable to create WebDriver for Platform :: "
+                    + this.platform.getName() + " method :: " + testMethodName);
+        }
+        extensionContext.getStore(STORE_NAMESPACE).put(testMethodName, webDriver);
+        return webDriver;
+    }
+
+    private WebDriver createWebDriver(String testMethodName, String[] specificCapabilities) {
+        WebDriver webDriver;
+        //TODO : Support for SpecificCapabilities
+        // webDriver = this.webDriverFactory.createWebDriverForPlatform(platform, testMethodName, specificCapabilities);
+        webDriver = this.webDriverFactory.createWebDriverForPlatform(platform, testMethodName);
+        return webDriver;
+    }
+
+}
---
+++ webdriver-framework-junit5/src/main/java/com/browserstack/webdriver/junit5/extensions/WebDriverTest.java
@@ -0,0 +1,30 @@
+package com.browserstack.webdriver.junit5.extensions;
+
+import org.apiguardian.api.API;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.*;
+
+import static org.apiguardian.api.API.Status.STABLE;
+
+/**
+ * {@code @WebDriverTest} is used to signal that the annotated method is a
+ * <em>web driver test</em> method.
+ *
+ * <p>{@code @WebDriverTest} methods must not be {@code private} or {@code static}
+ * and must not return a value.
+ *
+ * <p>{@code @WebDriverTest} methods may optionally declare {@link org.openqa.selenium.WebDriver WebDriver} parameter to be
+ * resolved by {@link com.browserstack.webdriver.junit5.extensions.WebDriverParameterResolver
+ * WebDriverParameterResolver}.
+ */
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@API(status = STABLE)
+@TestTemplate
+@ExtendWith(WebDriverTestExtension.class)
+public @interface WebDriverTest {
+    String[] capabilities() default {};
+}
