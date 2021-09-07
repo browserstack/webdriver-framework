786539a5980fd6d3411d9dc990468d1cdb313d3b
Abhi Singh <abhi.s@browserstack.com>
2021-09-07 18:48:12 +0530

    Fix Failure Reason message for BrowserStack

--- webdriver-framework-junit4/src/main/java/com/browserstack/webdriver/junit4/WebDriverProviderRule.java
+++ webdriver-framework-junit4/src/main/java/com/browserstack/webdriver/junit4/WebDriverProviderRule.java
@@ -2,6 +2,9 @@ package com.browserstack.webdriver.junit4;
 
 import java.net.MalformedURLException;
 import com.browserstack.webdriver.config.DriverType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 import org.openqa.selenium.JavascriptExecutor;
@@ -13,10 +16,10 @@ import com.browserstack.webdriver.core.WebDriverFactory;
 
 public class WebDriverProviderRule extends TestWatcher {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebDriverProviderRule.class);
-    private static final String TEST_STATUS_SCRIPT = "browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"%s\", \"reason\": \"%s\"}}";
     private String methodName;
     private WebDriver driver;
     private final WebDriverFactory webDriverFactory;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public WebDriverProviderRule() {
         this.webDriverFactory = WebDriverFactory.getInstance();
@@ -35,8 +38,9 @@ public class WebDriverProviderRule extends TestWatcher {
      */
     protected void succeeded(Description description) {
         LOGGER.info("Succeeded Test :: {} WebDriver Session :: {}", description.getDisplayName(), this.driver);
-        if(webDriverFactory.getDriverType() == DriverType.cloudDriver){
-            ((JavascriptExecutor) driver).executeScript(String.format(TEST_STATUS_SCRIPT, "passed", "Test Passed"));
+        if (webDriverFactory.getDriverType() == DriverType.cloudDriver) {
+            String script = createExecutorScript("passed", "Test Passed");
+            ((JavascriptExecutor) driver).executeScript(script);
         }
     }
 
@@ -45,11 +49,11 @@ public class WebDriverProviderRule extends TestWatcher {
      */
     protected void failed(Throwable e, Description description) {
         LOGGER.info("Failed Test :: {} WebDriver Session :: {}", description.getDisplayName(), this.driver, e);
-        if(webDriverFactory.getDriverType() == DriverType.cloudDriver){
-            ((JavascriptExecutor) driver).executeScript(String.format(TEST_STATUS_SCRIPT, "failed", e.getMessage()));
+        if (webDriverFactory.getDriverType() == DriverType.cloudDriver) {
+            String script = createExecutorScript("failed", e.getMessage());
+            ((JavascriptExecutor) driver).executeScript(script);
         }
     }
-
 
     @Override
     protected void starting(Description d) {
@@ -67,5 +71,29 @@ public class WebDriverProviderRule extends TestWatcher {
         }
     }
 
+    private String createExecutorScript(String status, String reason) {
+        ObjectNode rootNode = objectMapper.createObjectNode();
+        ObjectNode argumentsNode = objectMapper.createObjectNode();
+
+        // Read only the first line of the error message
+        reason = reason.split("\n")[0];
+        // Limit the error message to only 255 characters
+        if (reason.length() >= 255) {
+            reason = reason.substring(0, 255);
+        }
+
+        argumentsNode.put("status", status);
+        argumentsNode.put("reason", reason);
+
+        rootNode.put("action", "setSessionStatus");
+        rootNode.set("arguments", argumentsNode);
+        String executorStr = "";
+        try {
+            executorStr = objectMapper.writeValueAsString(rootNode);
+        } catch (JsonProcessingException e) {
+            throw new Error("Error creating JSON object for Marking tests", e);
+        }
+        return "browserstack_executor: " + executorStr;
+    }
 
 }
