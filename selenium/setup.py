c0fb3139b34922eaa53694c21640600127ed4a5d
princebaretto99 <princebaretto@gmail.com>
2022-01-22 01:16:34 +0530

    Added Package Data

--- selenium/setup.py
+++ selenium/setup.py
@@ -5,7 +5,7 @@ version='0.1',
 description='Webdriver Manager for Python',
 url='https://github.com/browserstack/webdriver-framework/tree/develop_webdriver-framework_python',
 author='Princeton Baretto',
-install_requires=['selenium==3.141.0'],
+install_requires=['selenium==3.141.0','browserstack-local','pyaml'],
 author_email='princeton.b@browserstack.com',
 packages=setuptools.find_packages(),
 zip_safe=False)
