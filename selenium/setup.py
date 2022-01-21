537d42b821b50a21d197e5d07cb42abba6a27594
princebaretto99 <princebaretto@gmail.com>
2022-01-22 00:59:36 +0530

    Added Package Data

---
+++ selenium/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='webdriverFrameworkCorePython',
+version='0.1',
+description='Webdriver Manager for Python',
+url='https://github.com/browserstack/webdriver-framework/tree/develop_webdriver-framework_python',
+author='Princeton Baretto',
+install_requires=['selenium==3.141.0'],
+author_email='princeton.b@browserstack.com',
+packages=setuptools.find_packages(),
+zip_safe=False)
